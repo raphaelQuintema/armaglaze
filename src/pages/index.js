c5b5dfa397302dd0bd1cec40a75364b951b9c39d
Raphael <raphvillejo@gmail.com>
2019-12-23 17:11:14 +0800

    second commit

---
+++ src/pages/index.js
@@ -0,0 +1,175 @@
+import React from "react"
+import Layout from "../components/layout"
+import abs1 from "../dist/images/abs1.jpg"
+import keys from "../dist/images/keys.jpg"
+import cylinder from "../dist/images/cylinder.jpg"
+import abs from "../dist/images/abs.jpg"
+import lockedin from "../dist/images/lockedin.jpg"
+
+const IndexPage = () => (
+  <Layout bg="container bg-yellow">
+    <div className="row bg-yellow">
+      <div className="col-lg-6 col-md-12 col-sm-12 bg-gun py-5"></div>
+      <div className="col-lg-6 col-md-12 col-sm-12 text-right">
+        <h1>HIGH SECURITY AVOCET LOCKS SUPPLIED & INSTALLED BY ARMAGLAZE</h1>
+        <button type="button" className="btn btn-dark">
+          LEARN MORE
+        </button>
+      </div>
+    </div>
+    <div className="row mt-3 mb-2">
+      <div className="col-lg-6 col-md-12 col-sm-12 bg-black abs-box text-white box-mr-1">
+        <p className="text-center">
+          <img src={abs1} className="img-fluid" alt="locks" />
+        </p>
+        <p>
+          Nightlatch Rim Cylinder with 3 keys and card Supplied & Fitted for
+          Just £79.00+ Vat
+        </p>
+
+        <button type="button" className="btn btn-dark">
+          <strong className="text-yellow">LEARN MORE</strong>
+        </button>
+      </div>
+      <div className="col-lg-6 col-md-12 col-sm-12 bg-black abs-box text-white box-ml-1">
+        <p className="text-center">
+          <img src={keys} className="img-fluid" alt="locks" />
+        </p>
+        <p>
+          Keyed alike cylinders ( French Doors ) Supplied & Fitted For Just
+          £130.00+ Vat
+        </p>
+
+        <button type="button" className="btn btn-dark">
+          <strong className="text-yellow">LEARN MORE</strong>
+        </button>
+      </div>
+    </div>
+    <div className="row">
+      <div className="col-lg-6 col-md-12 col-sm-12 bg-black abs-box text-white box-mr-1">
+        <p className="text-center">
+          <img src={cylinder} className="img-fluid" alt="locks" />
+        </p>
+
+        <p>
+          Cylinder with internal thumbturn, 3 keys & card Supplied & Fitted for
+          Just £88.00+ Vat
+        </p>
+
+        <button type="button" className="btn btn-dark">
+          <strong className="text-yellow">LEARN MORE</strong>
+        </button>
+      </div>
+      <div className="col-lg-6 col-md-12 col-sm-12 bg-black abs-box text-white box-ml-1">
+        <p className="text-center">
+          <img src={abs} className="img-fluid" alt="locks" />
+        </p>
+        <p>
+          Half Cylinder (no external access) Supplied & Fitted For Just £81.00+
+          Vat
+        </p>
+
+        <button type="button" className="btn btn-dark">
+          <strong className="text-yellow">LEARN MORE</strong>
+        </button>
+      </div>
+    </div>
+
+    <div className="row py-4">
+      <div className="col-lg-12 col-md-12 col-sm-12 mb-3">
+        <h2 className="text-center">Ultimate Security</h2>
+      </div>
+
+      <div className="col-lg-6 col-md-12 col-sm-12">
+        <ul>
+          <li>SS312 Sold Secure Diamond Grade</li>
+          <li>3 Star British Kitemark - TS007:2014 (KM 586135)</li>
+          <li>Secured by Design Accredited (Police preferred specification)</li>
+          <li>Patented Active Cam Technology - Internallock-off system</li>
+          <li>Pick, Drill & Bump Resistant</li>
+          <li>Pin-in-Pin and magnetic pin technology</li>
+          <li>6 Trap Pins for advanced pick resistance</li>
+          <li>18 Anti-drill zones</li>
+          <li>Double, single and thumbturn formats available</li>
+          <li>Three keys per cylinder</li>
+          <li>Secure online key registering</li>
+          <li>Keyed alike key/key pairs are available ex stock</li>
+          <li>Key alike with ABS euro cylinders, rim cylinders & padlocks</li>
+        </ul>
+      </div>
+      <div className="col-lg-6 col-md-12 col-sm-12 bg-tech"></div>
+    </div>
+    <div className="row">
+      <div className="col-lg-6 col-md-12 col-sm-12 bg-tech1"></div>
+      <div className="col-lg-6 px-5 bg-black abs-box text-white">
+        <p className="text-center">
+          <img src={lockedin} className="img-fluid" alt="locks" />
+        </p>
+        <p>
+          Nightlatch Rim Cylinder with 3 keys and card Supplied & Fitted for
+          Just £79.00+ Vat
+        </p>
+      </div>
+    </div>
+    <div className="row d-flex justify-content-center mt-4">
+      <div className="col-lg-12 col-md-12 col-sm-12 mb-3">
+        <h2 className="text-center">Contact Us</h2>
+      </div>
+      <div>
+        <form>
+          <div className="form-row">
+            <div className="form-group col-md-6">
+              <input
+                type="text"
+                className="form-control"
+                id="inputFullname"
+                placeholder="Your Name"
+              />
+            </div>
+            <div className="form-group col-md-6">
+              <input
+                type="email"
+                className="form-control"
+                id="inputEmail4"
+                placeholder="Email"
+              />
+            </div>
+          </div>
+          <div className="form-row">
+            <div className="form-group col-md-6">
+              <input
+                type="text"
+                className="form-control"
+                id="subject"
+                placeholder="Subject"
+              />
+            </div>
+            <div className="form-group col-md-6">
+              <input
+                type="text"
+                className="form-control"
+                id="inputEgroupmail4"
+                placeholder="Phone Number"
+              />
+            </div>
+          </div>
+          <div className="form-group">
+            <textarea
+              className="form-control"
+              id="exampleFormControlTextarea1"
+              rows="3"
+              placeholder="Message"
+            ></textarea>
+          </div>
+          <p className="text-center">
+            <button type="submit" className="text-center btn btn-dark">
+              Submit
+            </button>
+          </p>
+        </form>
+      </div>
+    </div>
+  </Layout>
+)
+
+export default IndexPage
---
+++ src/pages/locks/locks.js
@@ -0,0 +1,162 @@
+import React from "react"
+import Layout from "../components/layout"
+import { Link } from "gatsby"
+import abs1 from "../dist/images/abs1.jpg"
+import keys from "../dist/images/keys.jpg"
+import cylinder from "../dist/images/cylinder.jpg"
+import abs from "../dist/images/abs.jpg"
+import locks2 from "../dist/images/locks2.jpg"
+
+const Locks = () => (
+  <Layout bg="container bg-black">
+    <div className="row bg-yellow">
+      <div className="col-lg-12 col-md-12 col-sm-12 bg-black bg-key py-5"></div>
+    </div>
+    <div className="row py-4">
+      <div className="col-lg-6 col-md-12 col-sm-12 ">
+        <p className="text-center">
+          <img src={keys} className="img-fluid" width="580" alt="locks" />
+        </p>
+      </div>
+      <div className="col-lg-6 col-md-12 col-sm-12">
+        <h4>ABS ULTIMATE SERIES EURO CYLINDER LOCK - KEYED ALIKE PAIR</h4>
+        <br />
+        <p className="text-blue text-1">£110.00</p>
+        <br />
+        <p>
+          Simply choose your size, finish, key and lock quantities below, i.e.{" "}
+          <strong>1 = 1 Pair in the size you have chosen.</strong>
+        </p>
+        <br />
+        <p>
+          These keyed alike locks are sold in pairs as standard and each lock
+          comes with 3 keys. (6 in total).
+        </p>
+        <br />
+        <button type="button" className="btn btn-dark">
+          <strong className="text-yellow">CONTACT US</strong>
+        </button>
+      </div>
+    </div>
+
+    <div className="row py-4">
+      <div className="col-lg-6 col-md-12 col-sm-12">
+        <h4>ABS ULTIMATE SERIES NIGHTLATCH RIM CYLINDER</h4>
+        <br />
+        <p className="text-blue text-1">£40.00</p>
+        <br />
+        <p>
+          Simply choose your size, finish, key and lock quantities below, i.e.{" "}
+          <strong>1 = 1 Pair in the size you have chosen.</strong>
+        </p>
+        <br />
+        <p>
+          These keyed alike locks are sold in pairs as standard and each lock
+          comes with 3 keys. (6 in total).
+        </p>
+        <br />
+        <button type="button" className="btn btn-dark">
+          <strong className="text-yellow">CONTACT US</strong>
+        </button>
+      </div>
+      <div className="col-lg-6 col-md-12 col-sm-12 ">
+        <p className="text-center">
+          <img src={abs1} className="img-fluid" width="580" alt="locks" />
+        </p>
+      </div>
+    </div>
+
+    <div className="row py-4">
+      <div className="col-lg-6 col-md-12 col-sm-12 ">
+        <p className="text-center">
+          <img src={cylinder} className="img-fluid" width="580" alt="locks" />
+        </p>
+      </div>
+      <div className="col-lg-6 col-md-12 col-sm-12">
+        <h4>ABS ULTIMATE SERIES EURO CYLINDER LOCK - THUMBTURN (QUANTUM)</h4>
+        <br />
+        <p className="text-blue text-1">£60.00</p>
+        <br />
+        <p>
+          Abs Quantum Thumturn locks allow easy exit in case of an emergency and
+          its easy internal locking and unlocking function means you only need
+          to use the key when leaving and entering your house. Recommended by
+          emergency services
+        </p>
+        <br />
+        <button type="button" className="btn btn-dark">
+          <strong className="text-yellow">CONTACT US</strong>
+        </button>
+      </div>
+    </div>
+
+    <div className="row py-4">
+      <div className="col-lg-6 col-md-12 col-sm-12">
+        <h4>ABS ULTIMATE SERIES LOCKS HALF CYLINDER</h4>
+        <br />
+        <p className="text-blue text-1">£45.00</p>
+        <br />
+        <p>
+          Simply choose your size, finish, key and lock quantities below, i.e.{" "}
+          <strong>1 = 1 Pair in the size you have chosen.</strong>
+        </p>
+        <br />
+        <p>
+          These keyed alike locks are sold in pairs as standard and each lock
+          comes with 3 keys. (6 in total).
+        </p>
+        <br />
+        <button type="button" className="btn btn-dark">
+          <strong className="text-yellow">CONTACT US</strong>
+        </button>
+      </div>
+      <div className="col-lg-6 col-md-12 col-sm-12 ">
+        <p className="text-center">
+          <img src={abs} className="img-fluid" width="580" alt="locks" />
+        </p>
+      </div>
+    </div>
+    <div className="d-flex flex-column text-white pt-5 mb-3 border-bottom">
+      <ul className="d-flex flex-row no-list-type pl-5">
+        <li className="mr-5">
+          <Link className="text-yellow-link">FEATURES</Link>
+        </li>
+        <li className="mr-5">
+          <Link className="text-yellow-link">SPECIFICATIONS</Link>
+        </li>
+        <li>
+          <Link className="text-yellow-link">GUIDES & DOWNLOADS</Link>
+        </li>
+      </ul>
+    </div>
+    <div className="row d-flex justify-content-center mt-4">
+      <div className="col-lg-12 col-md-12 col-sm-12 mb-3">
+        <h2 className="text-left">FEATURES</h2>
+      </div>
+      <div>
+        <ul>
+          <li>
+            TS007 3 Star ratedout of the box - no need for cylinder guards or
+            security handles
+          </li>
+          <li>SS312 Sold Secure Diamond Accredited</li>
+          <li>No additional prep - easy retrofit</li>
+          <li>
+            Unique patented Snap Secure Technology - The Active Cam system locks
+            the cylinder after a snap attack to keep intruders out
+          </li>
+          <li>Anti-Snap, Pick, drill & bump resistant</li>
+          <li>British Standard Kitemark logo stamped</li>
+          <li>Police preferred Secured by Design accreditation</li>
+          <li>
+            Available in keyed alike, thumbturn, singles** & double profile
+            formats
+          </li>
+        </ul>
+        <img src={locks2} className="img-fluid" alt="locks" />
+      </div>
+    </div>
+  </Layout>
+)
+
+export default Locks
