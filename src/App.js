177231b6f4b59d7ff194b1bafca7a0b19bc51a3d
Raphael <raphvillejo@gmail.com>
2019-12-26 17:39:52 +0800

    fixed all bugs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
-import logo from "./logo.svg";
+import Home from "./pages/home/index";
+import Locks from "./pages/locks/locks";
+
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 
 function App() {
 	return (
-		<div className="App">
-			<h1>test</h1>
-		</div>
+		<BrowserRouter>
+			<Switch>
+				<Route exact path="/" component={Home} />
+				<Route exact path="/locks" component={Locks} />
+				<Route exact path="/contact" component={Home} />
+			</Switch>
+		</BrowserRouter>
 	);
 }
 
